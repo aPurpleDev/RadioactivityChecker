1b84d26ea410afd072ece3d0e557ff23961403f5
aPurpleDev <46002088+aPurpleDev@users.noreply.github.com>
2019-04-10 13:25:52 +0200

    In progress exercise with mentor to improve on Development practices (TDD / SOLID)

--- src/main/java/frradioactivity/Mapcheck.java
+++ src/main/java/frradioactivity/Mapcheck.java
@@ -4,59 +4,55 @@ public class Mapcheck { //One purpose. SRP, Single Responsibility Principle.
 
     enum RadioColor { //Enum to return as per exercise instructions
 
-        GREEN(50, "Green"), //radioactivityIndex is the color's limit per radioactivity.
-        YELLOW(100, "Yellow"), //the colorname is just a String containing the name's color
-        ORANGE(150, "Orange"),
-        RED(200, "Red");
+        GREEN(0,50), //radioactivityIndex is the color's limit per radioactivity.
+        YELLOW(51,100), //the colorname is just a String containing the name's color
+        ORANGE(101,150),
+        RED(151,Integer.MAX_VALUE);
 
-        private int radioactivityIndex;
-        private String colorname;
+        private final int minRadioIndex;
+        private final int maxRadioIndex;
 
-        RadioColor(int radioactivityIndex, String colorname) { //Constructor to enable the enum attributes
-            if (radioactivityIndex <= 50) {
-                this.radioactivityIndex = 50;
-                this.colorname = "Green";
-            } else if (radioactivityIndex <= 100) {
-                this.radioactivityIndex = 100;
-                this.colorname = "Yellow";
-            } else if (radioactivityIndex <= 150) {
-                this.radioactivityIndex = 150;
-                this.colorname = "Orange";
-            } else {
-                this.radioactivityIndex = 200;
-                this.colorname = "Red";
-            }
+        RadioColor(int minRadioIndex, int maxRadioIndex) {
+
+            this.minRadioIndex = minRadioIndex;
+            this.maxRadioIndex = maxRadioIndex;
         }
+
     }
 
     class EnumChecker{ //contains the method to return the right enum per radioactivityindex
 
-        EnumChecker(){
-        Mapcheck.RadioColor[] color = Mapcheck.RadioColor.values(); //array to browse the Enum values
+        public RadioColor checkRadioColor(int deptRadioactivity){
+
+            for (RadioColor value : RadioColor.values()) {
+                if(value.minRadioIndex <= deptRadioactivity && value.maxRadioIndex >= deptRadioactivity) {
+                    return value;
+                }
+            }
+            throw new IllegalArgumentException(); //Pour sécuriser la méthode
         }
 
-        public RadioColor checkRadioColor(int deptRadioactivity) {
+        public RadioColor checkRadioColor2(int deptRadioactivity) {
 
             RadioColor colorreturn = null;
 
                 if(deptRadioactivity <= 50) {
 
-                    colorreturn = RadioColor.GREEN;
+                    return RadioColor.GREEN; //retourne une instance de l'enum
 
-                } else if (deptRadioactivity <= 100) {
+                }
+                if (deptRadioactivity <= 100) {
 
-                    colorreturn = RadioColor.YELLOW;
+                    return RadioColor.YELLOW;
 
-                } else if (deptRadioactivity <= 150) {
+                }
+                if (deptRadioactivity <= 150) {
 
-                    colorreturn = RadioColor.ORANGE;
+                    return RadioColor.ORANGE;
 
-                } else {
-
-                    colorreturn = RadioColor.RED;
                 }
 
-            return colorreturn; //returned Enum of right color type
+                return RadioColor.RED;
         }
     }
 }
--- src/test/java/frradioactivity/checkRadioColorTest.java
+++ src/test/java/frradioactivity/checkRadioColorTest.java
@@ -9,7 +9,37 @@ public class checkRadioColorTest {
     private final Mapcheck.EnumChecker enumChecker = mapcheck.new EnumChecker();
 
     @Test
+    public void green() {
+        Assertions.assertThat(enumChecker.checkRadioColor(0)).isEqualTo(Mapcheck.RadioColor.GREEN);
+    }
+
+    @Test
+    public void green2() {
+        Assertions.assertThat(enumChecker.checkRadioColor(15)).isEqualTo(Mapcheck.RadioColor.GREEN);
+    }
+
+    @Test
+    public void green3() {
+        Assertions.assertThat(enumChecker.checkRadioColor(50)).isEqualTo(Mapcheck.RadioColor.GREEN);
+    }
+
+    @Test
+    public void yellow() {
+        Assertions.assertThat(enumChecker.checkRadioColor(51)).isEqualTo(Mapcheck.RadioColor.YELLOW);
+        Assertions.assertThat(enumChecker.checkRadioColor(75)).isEqualTo(Mapcheck.RadioColor.YELLOW);
+        Assertions.assertThat(enumChecker.checkRadioColor(100)).isEqualTo(Mapcheck.RadioColor.YELLOW);
+
+    }
+    @Test
     public void orange() {
-        Assertions.assertThat(enumChecker.checkRadioColor(120)).isEqualTo(Mapcheck.RadioColor.ORANGE);
-       }
+        Assertions.assertThat(enumChecker.checkRadioColor(101)).isEqualTo(Mapcheck.RadioColor.ORANGE);
+        Assertions.assertThat(enumChecker.checkRadioColor(125)).isEqualTo(Mapcheck.RadioColor.ORANGE);
+        Assertions.assertThat(enumChecker.checkRadioColor(150)).isEqualTo(Mapcheck.RadioColor.ORANGE);
+    }
+    @Test
+    public void red() {
+        Assertions.assertThat(enumChecker.checkRadioColor(151)).isEqualTo(Mapcheck.RadioColor.RED);
+        Assertions.assertThat(enumChecker.checkRadioColor(175)).isEqualTo(Mapcheck.RadioColor.RED);
+        Assertions.assertThat(enumChecker.checkRadioColor(200)).isEqualTo(Mapcheck.RadioColor.RED);
+    }
 }
